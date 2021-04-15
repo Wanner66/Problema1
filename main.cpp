1e359a5e6555c29909fbf9c0852220ebd86916be
Wanner <>
2021-04-15 16:08:39 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  int A,B,H;
+  cout << "Digite la Base: "<<endl;
+  cin >> B;
+  cout << "Digite la Altura: "<<endl;
+  cin >> H;
+  A=B*H;
+  cout << "El Area del triangulo es: "<< endl << A;
+}
